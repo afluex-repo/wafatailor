acb9780d26e0d3602744c9181c0cd758958622d5
Sudhakar Sharma <developer2.afluex@gmail.com>
2022-07-01 15:58:23 +0530

    changes

--- WafaTailor/Models/SaleOrder.cs
+++ WafaTailor/Models/SaleOrder.cs
@@ -11,6 +11,7 @@ namespace WafaTailor.Models
     {
         public string SaleOrderId { get; set; }
         public string Fk_UserId { get; set; }
+        public string Fk_ShopeId { get; set; }
         public string Description { get; set; }
         public string Amount { get; set; }
         public string OrderDate { get; set; }
@@ -18,21 +19,30 @@ namespace WafaTailor.Models
         public string AddedBy { get; set; }
         public string LoginId { get; set; }
         public string Result { get; set; }
-        
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+
+
+
         public DataTable dtSaleOrderDetails { get; set; }
-        
+
         public DataSet SaveSaleOrderDetails()
         {
             SqlParameter[] para =
             {
-                new SqlParameter("@dtSaleOrderDetails",dtSaleOrderDetails),
+                    new SqlParameter("@Fk_UserId",Fk_UserId),
+                   new SqlParameter("@Fk_ShopeId",Fk_ShopeId),
+                   new SqlParameter("@dtSaleOrderDetails",dtSaleOrderDetails),
                  new SqlParameter("@AddedBy",AddedBy)
             };
             DataSet ds = DBHelper.ExecuteQuery("SaveSaleOrderDetails", para);
             return ds;
         }
 
-        public DataSet GetDetails()
+        public DataSet GetUserDetails()
         {
             SqlParameter[] para =
             {
@@ -41,6 +51,6 @@ namespace WafaTailor.Models
             DataSet ds = DBHelper.ExecuteQuery("GetUserDetails", para);
             return ds;
         }
-        
+
     }
 }
