e64c73826fe9d6d31760c402d238768934a57459
Ashwani Mishra <developer6.afluex@gmail.com>
2022-07-28 16:52:45 +0530

    changes

--- WafaTailor/Controllers/AdminController.cs
+++ WafaTailor/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WafaTailor.Filter;
 using WafaTailor.Models;
 
 namespace WafaTailor.Controllers
@@ -129,5 +130,134 @@ namespace WafaTailor.Controllers
             return View(model);
         }
 
+        public ActionResult BillEntry(Admin model)
+        {
+            #region Shop
+            List<SelectListItem> ddlShop = new List<SelectListItem>();
+            DataSet ds1 = model.GetShopNameDetails();
+            if (ds1 != null && ds1.Tables.Count > 0 && ds1.Tables[0].Rows.Count > 0)
+            {
+                int count = 0;
+                foreach (DataRow r in ds1.Tables[0].Rows)
+                {
+                    if (count == 0)
+                    {
+                        ddlShop.Add(new SelectListItem { Text = "Select Shop", Value = "0" });
+                    }
+                    ddlShop.Add(new SelectListItem { Text = r["ShopName"].ToString(), Value = r["Pk_ShopId"].ToString() });
+                    count++;
+                }
+            }
+            ViewBag.ddlShop = ddlShop;
+            #endregion
+            #region Customer
+            List<SelectListItem> ddlcustomer = new List<SelectListItem>();
+            DataSet ds = model.GetCustomerDetails();
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                int count = 0;
+                foreach (DataRow r in ds.Tables[0].Rows)
+                {
+                    if (count == 0)
+                    {
+                        ddlcustomer.Add(new SelectListItem { Text = "Select Customer", Value = "0" });
+                    }
+                    ddlcustomer.Add(new SelectListItem { Text = r["CustomerName"].ToString(), Value = r["PK_UserId"].ToString() });
+                    count++;
+                }
+            }
+            ViewBag.ddlcustomer = ddlcustomer;
+            #endregion
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("BillEntry")]
+        [OnAction(ButtonName = "SaveBill")]
+        public ActionResult BillEntryAction(Admin model)
+        {
+            try
+            {
+                model.AddedBy = Session["Pk_EmployeeId"].ToString();
+                DataSet ds = new DataSet();
+                ds = model.SaveBillEntry();
+                if (ds != null  && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0][0].ToString() == "1")
+                    {
+                        TempData["BillEntry"] = "Bill Entry saved Successfully !!";
+                    }
+                    else if (ds.Tables[0].Rows[0][0].ToString() == "0")
+                    {
+                        TempData["BillEntry"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                    }
+                }
+                else
+                {
+                    TempData["BillEntry"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["BillEntry"] = ex.Message;
+            }
+            return RedirectToAction("BillEntry", "Admin");
+        }
+
+        public ActionResult BillList(Admin model)
+        {
+            List<Admin> lst = new List<Admin>();
+            DataSet ds = model.GetBillDetails();
+            if (ds != null && ds.Tables[0].Rows.Count > 0 && ds.Tables.Count > 0)
+            {
+                foreach (DataRow r in ds.Tables[0].Rows)
+                {
+                    Admin obj = new Admin();
+                    obj.BillId = r["Pk_BillId"].ToString();
+                    obj.ShopName = r["ShopName"].ToString();
+                    obj.NoOfPiece = r["NoOfPiece"].ToString();
+                    obj.OriginalPrice = r["OriginalPrice"].ToString();
+                    obj.BillNo = r["BillNo"].ToString();
+                    obj.BillDate = r["BillDate"].ToString();
+                    lst.Add(obj);
+                }
+                model.lstList = lst;
+            }
+            return View(model);
+        }
+
+        public ActionResult PrintBill(string BillId)
+        {
+            List<Admin> lstbill = new List<Admin>();
+            Admin model = new Admin();
+            model.BillId = BillId;
+            DataSet ds = model.PrintBill();
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                ViewBag.CustomerName = ds.Tables[0].Rows[0]["Name"].ToString();
+                ViewBag.CustomerMobile = ds.Tables[0].Rows[0]["Mobile"].ToString();
+                ViewBag.CustomerAddress = ds.Tables[0].Rows[0]["Address"].ToString();
+                ViewBag.Email = ds.Tables[0].Rows[0]["Email"].ToString();
+                ViewBag.BillNo = ds.Tables[0].Rows[0]["BillNo"].ToString();
+            }
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables[1].Rows.Count > 0)
+            {
+                foreach (DataRow r in ds.Tables[1].Rows)
+                {
+                    model.BillDate = ds.Tables[1].Rows[0]["BillDate"].ToString();
+                    model.Advance = ds.Tables[1].Rows[0]["AdvanceAmount"].ToString();
+                    model.NoOfPiece = ds.Tables[1].Rows[0]["NoOfPiece"].ToString();
+                    model.OriginalPrice = ds.Tables[1].Rows[0]["OriginalPrice"].ToString();
+                    model.Discount = ds.Tables[1].Rows[0]["Discount"].ToString();
+                    model.FinalPrice = ds.Tables[1].Rows[0]["FinalAmount"].ToString();
+                    lstbill.Add(model);
+                }
+                model.lstList = lstbill;
+                ViewBag.FinalPrice = double.Parse(ds.Tables[1].Compute("sum(FinalPrice)", "").ToString()).ToString("n2");
+            }
+
+            return View(model);
+        }
+        
     }
 }
